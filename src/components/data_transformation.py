31810a707e6a587f10530963f4e817d9784c9b19
Venkatesh_Chintha <chinthavenkatesh2523@gmail.com>
2024-09-17 22:56:01 +0530

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -93,7 +93,6 @@ class DataTransformation:
 
             logging.info("Applying preprocessing object on training dataframe and testing dataframe")
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
